b8968071a5368379bfe6701d9807c573336a3669
Cameron Norman <cameron.norman@tourlane.com>
2020-06-24 16:22:28 +0200

    added validations

--- app/models/wash_type.rb
+++ app/models/wash_type.rb
@@ -1,2 +1,5 @@
 class WashType < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true, numericality: true
+  validates :cost, presence: true, numericality: true
 end
