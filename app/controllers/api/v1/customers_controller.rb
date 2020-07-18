f189d8a15ea87e3c77818137e0c4bc0d410d4be9
Cameron Norman <cameron.norman@tourlane.com>
2020-07-18 19:18:29 +0200

    added a search endpoint

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,4 +1,18 @@
 class Api::V1::CustomersController < Api::V1::ApiController
+
+  def index
+    if params["contact_number"]
+      @instances = model.where("contact_number ILIKE ?", "%#{params['contact_number']}%")
+    end
+
+    if params["registration_number"]
+      @instances = model.joins(:vehicles).where("vehicles.registration_number ILIKE ?", "%#{params['registration_number']}%")
+    end
+
+    headers['X-Instance-Total'] = instances.count
+    render json: paginated_instances
+  end
+
   private
 
   def model
