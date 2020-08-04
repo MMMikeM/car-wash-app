259f9fadb7a14894a8befd300bcb698e1d8387ae
Cameron Norman <cameron.norman@tourlane.com>
2020-08-04 14:30:48 +0200

    return report in rands instead of cents

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::ReportsController < Api::V1::ApiController
     Wash
       .joins(:wash_type)
       .where("washes.created_at >= ? AND washes.created_at <= ?", permitted_params[:start_date], permitted_params[:end_date])
-      .select("wash_types.id, wash_types.name, SUM(wash_types.cost) as cost, SUM(wash_types.price) as price, count(washes.*) as wash_count")
+      .select("wash_types.id, wash_types.name, (SUM(wash_types.cost) / 100) as cost, (SUM(wash_types.price) / 100) as price, count(washes.*) as wash_count")
       .group("wash_types.name, wash_types.id")
   end
 
