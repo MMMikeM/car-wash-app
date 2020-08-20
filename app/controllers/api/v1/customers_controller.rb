ff375909149f3b96687e962f91e9a1cef4ae432b
Cameron Norman <cameron.norman@tourlane.com>
2020-08-20 08:48:28 +0200

    allows update of total points

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -80,6 +80,7 @@ class Api::V1::CustomersController < Api::V1::ApiController
   def permitted_params
     params.require(:customer)
           .permit(:name, :email, :contact_number, :opted_for_marketing, vehicles_attributes: [:registration_number])
+          .permit(:name, :email, :contact_number, :opted_for_marketing, :total_points, vehicles_attributes: [:registration_number])
           .merge({
             :password => password,
             :password_confirmation => password
